620f3d8d1eeba7d8989694775f020f2ade98a802
Arunav Saikia <arunavsk1@gmail.com>
2020-03-19 19:25:02 -0400

    more functionality

--- Shiny App/app.R
+++ Shiny App/app.R
@@ -3,35 +3,53 @@ library(shinythemes)
 ui <- fluidPage(
   theme = shinytheme("darkly"),
   navbarPage("App Title",
-             tabPanel("Plot"),
-             tabPanel("About"),
+             tabPanel("Plots"),
+             tabPanel("Theory"),
              navbarMenu("More",
-                        tabPanel("Summary"),
+                        tabPanel("About"),
                         "----",
-                        "Section header",
-                        tabPanel("Table")
+                        "Others",
+                        tabPanel("FAQs"),
+                        tabPanel("Resources")
              )
   ),
   titlePanel("Understanding Noise Processes", windowTitle = 'Understanding Noise Processes'),
   'What the app is about',
   sidebarLayout(
     sidebarPanel(
+      sliderInput('n', '# datapoints', value = 100, min = 50, max = 1000, step = 20),
       numericInput('p', '# AR components (p)',value = 1, min = 0),
-      numericInput('q', '# MA components (q)',value = 1, min = 0),
-      uiOutput("ARCoeff"),
-      uiOutput("MACoeff"),
-      radioButtons("typeInput", "Time Domain/Frequency Domain",
-                   choices = c("Time Domain", "Frequency Domain"),
-                   selected = "Time Domain")
+      numericInput('q', '# MA components (q)',value = 0, min = 0),
+      conditionalPanel(
+        condition = "input.p != 0",
+        uiOutput("ARCoeff")
+      ),
+      conditionalPanel(
+        condition = "input.q != 0",
+        uiOutput("MACoeff")
+      ),
+      # radioButtons("typeInput", "Time Domain/Frequency Domain",
+      #              choices = c("Time Domain", "Frequency Domain"),
+      #              selected = "Time Domain")
                  ),
     mainPanel(
-      plotOutput("TS"),
-      br(),br(),
-      plotOutput("acf"))
+      tabsetPanel(
+        tabPanel("Raw Signal", plotOutput("TS"), textOutput('x')),
+        tabPanel("Time Domain", plotOutput("acf"),
+                 br(),br(),
+                 plotOutput("pacf")),
+        tabPanel("Spectral Analysis", 
+                 sliderInput('filtersize', 'Filter size', value = 20, min = 10, max = 50, step = 5),
+                 class = 'rightAlign',
+                 plotOutput("spec"))
+      )
   )
+)
 )
 server <- function(input, output) {
   output$ARCoeff <- renderUI({
+    
+    
     lapply(1:input$p, function(i) {
       numericInput(paste('alpha',i,sep=''), paste('alpha',i) ,value = 0.5, min = 0)
     })
@@ -42,27 +60,58 @@ server <- function(input, output) {
       numericInput(paste('beta',i,sep=''), paste('beta',i) ,value = 0.5, min = 0)
     })
   })
-  
+
   arcoeff <- reactive({
-    vec1 = c()
-    lapply(1:input$p, function(i) {
-      vec1[i]<- paste0('input.beta',i)
-    })
-    print(vec1)
-    return(vec1)
+    if (input$p == 0) {
+      return(c(0))
+    } else {
+      coeff <- c()
+      lapply(1:input$p, function(i) {
+        coeff[i] = input[[paste0('alpha', as.character(i))]]
+      })
+      return(coeff)
+    }
   })
+  
+  macoeff <- reactive({
+    if (input$q == 0) {
+      return(c(0))
+    } else {
+      coeff <- c()
+      lapply(1:input$q, function(i) {
+        coeff[i] = input[[paste0('beta', as.character(i))]]
+      })
+      return(coeff)
+    }
+  })
+  
+  # reactive({print(macoeff())})
+  # reactive({print(arcoeff())})
   TSdata  <- reactive({
-    arima.sim(n = 100, model = list(order = c(input$p, 0, 0), 
-                                    ar = input$alpha1,
-                                    sd = 0.1))
+    arima.sim(n = input$n, model = list( 
+                                    ar = arcoeff(),
+                                    ma = macoeff()
+                                    ),
+                                    sd = 0.1)
   })
+  
+  output$x <- renderText({arcoeff()})
   
   output$TS <- renderPlot({
     plot(TSdata(), type = 'l')
   })
   
   output$acf <- renderPlot({
-  acf(TSdata(), 20)
+    acf(TSdata(), 20)
+  })
+  
+  output$pacf <- renderPlot({
+    pacf(TSdata(), 20)
+  })
+  output$spec <- renderPlot({
+    spec.pgram(TSdata(),spans=c(input$filtersize,input$filtersize),taper=0,log="no")
   })
   }
+
+
 shinyApp(ui = ui, server = server)
